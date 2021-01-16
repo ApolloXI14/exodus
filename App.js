35077394cc9b70a8eaa9ed63b533a69e79326f76
Sean McHugh <sean.a.mchugh@gmail.com>
2021-01-16 17:26:28 -0500

    Setup for webpack/App.js/index/main/package.json

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+//import Navbar from './components/Navbar';
+import { Route, Switch, withRouter } from 'react-router-dom';
+
+
+
+class App extends Component{
+   render(){
+      return(
+         <div className="intro">
+         	<h1>EXODUS</h1>
+         </div>
+      );
+   }
+}
+
+export default App;

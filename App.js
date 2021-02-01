e7ebef21d4b42e41d5379fd0f28f43b70484889e
Sean McHugh <sean.a.mchugh@gmail.com>
2021-02-01 01:13:32 -0500

    Added logo and World/Story/Gallery/Community images to homepage; styling

--- App.js
+++ App.js
@@ -8,8 +8,15 @@ class App extends Component{
    render(){
       return(
          <div className="intro">
-         	<h1>EXODUS</h1>
-            <img src="www/img/EXODUS_LOGO_BG01-1024x461.png" />
+            <div id="logoDiv">
+               <img id="exodusLogo" src="www/img/EXODUS_LOGO_BG01-1024x461.png" />
+            </div>
+            <div id="imgsDiv-flex">
+               <img id="exodusLogo" src="www/img/World-Link-Image.png" />
+               <img id="exodusLogo" src="www/img/Story-Link-Image.png" />
+               <img id="exodusLogo" src="www/img/Gallery-Link-Image.png" />
+               <img id="exodusLogo" src="www/img/Community-Link-Image.png" />
+            </div>
          </div>
       );
    }

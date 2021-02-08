18e1d4696960bcd8d03fa5d57a0284ded2bc7672
Dustin Cintron <D.Cint88@gmail.com>
2021-02-07 20:12:13 -0500

    Import bootstrap and navbar to App.js

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-//import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 import { Route, Switch, withRouter } from 'react-router-dom';
-
-
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component{
    render(){
       return(
          <div className="intro">
+            <Navbar />
          	<h1>EXODUS</h1>
             <img src="www/img/EXODUS_LOGO_BG01-1024x461.png" />
          </div>

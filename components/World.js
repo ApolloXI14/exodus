6e234f440e28ab69d0e2518de913453e644b3a29
Dustin Cintron <D.Cint88@gmail.com>
2021-02-12 18:36:19 -0500

    World carousel initial

--- components/World.js
+++ components/World.js
@@ -1,21 +1,118 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+const planets = [
+  {
+    src: "../www/img/background/Earth-world-concept.jpg",
+    caption: "Earth",
+    altText: "Planet Earth",
+    url: "/world/earth"
+  },
+  {
+    src: "../www/img/background/Exodus-world-concept.jpg",
+    caption: "Exodus",
+    altText: "Planet Exodus",
+    url: "/world/exodus"
+  }
+];
 
 class World extends Component {
+  constructor(props) {
+    super(props);
+    this.nextSlide = this.nextSlide.bind(this);
+    this.prevSlide = this.prevSlide.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.state = {
+      activeIndex: 0,
+      animating: false
+    };
+  };
+
+  nextSlide() {
+    const { animating, activeIndex } = this.state;
+    if(animating) return;
+    const nextIndex = activeIndex === planets.length - 1 ? 0 : activeIndex + 1;    
+    this.setState({
+      activeIndex: nextIndex
+    });
+  };
+
+  prevSlide() {
+    const { animating, activeIndex } = this.state;
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? planets.length - 1 : activeIndex - 1;
+    this.setState({
+      activeIndex: nextIndex
+    });
+  };
+
+  goToIndex(newIndex) {
+    const { animating } = this.state;
+    if (animating) return;
+    this.setState({
+      activeIndex: newIndex
+    });
+  };
+
   render() {
+    const slides = planets.map(planet => {
+      return (
+          <CarouselItem
+            onExiting={() => this.setState({ animating: true })}
+            onExited={() => this.setState({ animating: false })}
+            key={planet.src}
+          > 
+            <Link to={planet.url}>
+              <img
+                src={planet.src}
+                alt={planet.altText}
+                style={styles.image}
+              />
+            </Link>
+            <CarouselCaption captionHeader={planet.caption} />
+          </CarouselItem>
+      );
+    });
+
     return (
-      <div>
-        <h1>World component</h1>
-        <h1>World component</h1>
-        <h1>World component</h1>
-        <h1>World component</h1>
-        <h1>World component</h1>
-        <h1>World component</h1>
-        <h1>World component</h1>
-        <h1>World component</h1>
-        <h1>World component</h1>
+      <div style={styles.carousel}>
+        <h1 className="text-center">Choose your Path</h1>
+        <Carousel
+          activeIndex={this.state.activeIndex}
+          next={this.nextSlide}
+          previous={this.prevSlide}
+        >
+          <CarouselIndicators
+            items={planets}
+            activeIndex={this.state.activeIndex}
+            onClickHandler={this.goToIndex}
+          />
+          {slides}
+          <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.prevSlide} />
+          <CarouselControl direction="next" directionText="Next" onClickHandler={this.nextSlide} />
+        </Carousel>
       </div>
     );
   };
 };
+
+const styles = {
+  image: {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "40%"
+  },
+  carousel: {
+    marginTop: "10px"
+  }
+}
 
 export default World;

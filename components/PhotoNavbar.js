4e92d37e15c4b3194619b11976b4caafda420fb6
Sean McHugh <sean.a.mchugh@gmail.com>
2021-02-21 16:39:52 -0500

    "showNavbar" flag

--- components/PhotoNavbar.js
+++ components/PhotoNavbar.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class PhotoNavbar extends React.PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			showNavbar: this.props.showNavbar
+		};
+	}
 	componentDidMount() {
+		
 	 }
 	render() {
 		return (
-			<div id="imgsDiv-flex" style={{display: "flex"}}>
+			<div id="imgsDiv-flex" style= {{display: this.props.showNavbar ? "flex" : "none"}}>
                <Link to="/world"> <img id="exodusLogo" src="www/img/World-Link-Image.png" /></Link>
                <Link to="/story"> <img id="exodusLogo" src="www/img/Story-Link-Image.png" /></Link>
                <Link to="/gallery"><img id="exodusLogo" src="www/img/Gallery-Link-Image.png" /></Link>

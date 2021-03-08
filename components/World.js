fe94741a2f42facc49d93f180586a9d77267eddb
Dustin Cintron <D.Cint88@gmail.com>
2021-03-07 20:20:45 -0500

    Changed Dual world Images

--- components/World.js
+++ components/World.js
@@ -3,6 +3,38 @@ import React, { Component } from 'react';
 class World extends Component {
   render() {
     return (
+      <div>
+        <img
+          src="../www/img/background/Earth-world-concept.jpg"
+          style={{
+            clipPath: "polygon(100% 0%, 0% 100%, 0% 0%)",
+            width: "100%",
+            position: "absolute",
+          }}
+        />
+        <img 
+          src="../www/img/background/Exodus-world-concept.jpg"
+          style={{
+            clipPath: "polygon(100% 0%, 0% 100%, 100% 100%)",
+            width: "100%",
+            position: "relative",
+          }}
+        />
+      </div>
+    );
+  };
+};
+
+export default World;
+
+
+
+/* Old Component, two images diagonal split, but not responsive
+import React, { Component } from 'react';
+
+class World extends Component {
+  render() {
+    return (
       <div className='pageOption'>
         <a href='/earth' className='option' data-inf='U.S.B.'>
           <img src="../www/img/background/Earth-world-concept.jpg" width="800px" />
@@ -16,3 +48,4 @@ class World extends Component {
 };
 
 export default World;
+*/

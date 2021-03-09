a0a1ca44afc3bb7d2223f391bd636ade9e802dba
Dustin Cintron <D.Cint88@gmail.com>
2021-03-09 16:52:53 -0500

    Appearance of World done; Bug persists

--- components/World.js
+++ components/World.js
@@ -5,12 +5,21 @@ class World extends Component {
   render() {
     return (
       <div className="world-container">
-        <a href='/earth' data-inf='U.S.B.'>
-          <img src="../www/img/background/Earth-world-concept.jpg" className="top-image" />
-        </a>
-        <a href='/exodus' data-inf='Exodus'>
-          <img src="../www/img/background/Exodus-world-concept.jpg" className="bottom-image" />
-        </a>
+
+        <div className="top-image-container">
+          <div className="top-image-text">U.S.B.</div>
+          <a href='/earth'>
+            <img src="../www/img/background/Earth-world-concept.jpg" className="top-image" />
+          </a>
+        </div>
+
+        <div className="bottom-image-container">
+          <div className="bottom-image-text">Exodus</div>
+          <a href='/exodus'>
+            <img src="../www/img/background/Exodus-world-concept.jpg" className="bottom-image" />
+          </a>
+        </div>
+
       </div>
     );
   };

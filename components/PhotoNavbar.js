eb8b9e2dc824fed9491770e1531db7e59761a59c
Sean McHugh <sean.a.mchugh@gmail.com>
2021-04-04 18:09:05 -0400

    Enhanced img src references to use webpack DefinePlugin variable

--- components/PhotoNavbar.js
+++ components/PhotoNavbar.js
@@ -5,10 +5,10 @@ class PhotoNavbar extends React.PureComponent {
 	render() {
 		return (
 			<div id="imgsDiv-flex" style={{display: "flex"}}>
-               <Link to="/world"> <img id="exodusLogo" src="www/img/World-Link-Image.png" /></Link>
-               <Link to="/story"> <img id="exodusLogo" src="www/img/Story-Link-Image.png" /></Link>
-               <Link to="/gallery"><img id="exodusLogo" src="www/img/Gallery-Link-Image.png" /></Link>
-               <Link to="/community"> <img id="exodusLogo" src="www/img/Community-Link-Image.png" /></Link>
+               <Link to="/world"> <img id="exodusLogo" src={IMGS_DIR + "World-Link-Image.png"} /></Link>
+               <Link to="/story"> <img id="exodusLogo" src={IMGS_DIR + "Story-Link-Image.png"} /></Link>
+               <Link to="/gallery"><img id="exodusLogo" src={IMGS_DIR + "Gallery-Link-Image.png"} /></Link>
+               <Link to="/community"> <img id="exodusLogo" src={IMGS_DIR + "Community-Link-Image.png" } /></Link>
             </div>);
 	}
 }
